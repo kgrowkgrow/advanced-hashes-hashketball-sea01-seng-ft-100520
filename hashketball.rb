3e66ecbf507572ec81148fddf29d7b7c6e5339ce
Kevin Grow <kevingrow11@gmail.com>
2020-09-29 19:11:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -176,7 +176,7 @@ def player_numbers(team_name)
   team_numbers = []
   game_hash.each do |location, team_data|
     if team_data[:team_name] == team_name
-      team_data.each do |name_hash|
+      team_data.each do |name_array|
         binding.pry
         team_numbers.push(name_hash[:team_name])
       end 
