3480177f518417a8960731065f34a0099be94525
Kevin Grow <kevingrow11@gmail.com>
2020-09-29 18:03:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -130,10 +130,8 @@ end
 def num_points_scored(player)
   game_hash.each do |team, tcp_hash|            #remember to return right value cuz each won't
     tcp_hash.each do |tcp_key, tcp_value|
-      binding.pry
       if tcp_value == :players #next is array
        binding.pry
-      puts "kevin"
       end 
     end 
   end 
