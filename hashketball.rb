8afae0e4f10006659078ec34f13afcebbcad9939
Kevin Grow <kevingrow11@gmail.com>
2020-09-29 19:31:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -200,6 +200,8 @@ def big_shoe_rebounds
   rebounds = nil
   game_hash.each do |location, team_data|
     team_data[:players].each do |attribute|
+      if attribute[:shoe] > 
+      end 
     end 
   end 
 end 
