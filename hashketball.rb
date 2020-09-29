5de68e50bbe4297a1e37559fc13a2558356a66c2
Kevin Grow <kevingrow11@gmail.com>
2020-09-29 18:23:23 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -131,8 +131,8 @@ end
 def num_points_scored(player_name)
   player_list = game_hash[:home][:players]
   binding.pry
-  game_hash.each do |location, team_data|
-    team_data.each do |attribute, data|
+  game_hash.map |location, team_data|
+    team_data.map |attribute, data|
       if data == player_list
         puts 'deoeoeoeoeo'
       end 
