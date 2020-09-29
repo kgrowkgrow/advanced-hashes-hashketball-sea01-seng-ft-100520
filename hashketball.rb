024cdfa86e4c7c9c3859b37bf64d5e4cb4dd5dcf
Kevin Grow <kevingrow11@gmail.com>
2020-09-29 19:27:53 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -188,6 +188,7 @@ def player_stats(player_name)
   stats = nil
   game_hash.each do |location, team_data|
     team_data[:players].each do |attribute|
+      if attribute[:player_name] == player_name
       stats = attribute
     end 
   end 
